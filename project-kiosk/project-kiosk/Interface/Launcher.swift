305ab637bec1749e987da847dc2e2045ff16724e
“hw3590” <lkhs44@naver.com>
2023-07-25 15:42:28 +0900

    ""

---
+++ project-kiosk/project-kiosk/Interface/Launcher.swift
@@ -0,0 +1,42 @@
+//
+//  Launcher.swift
+//  project-kiosk
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+
+import Foundation
+
+let manual = Manual()
+let layout = Layout()
+
+func app() {
+	//MARK: Run console without debug build
+	print(manual.on)
+	print(layout.shrink())
+	print(manual.selectMenu)
+	layout.showMenu()
+	while true {
+		guard let selected = readLine()?.uppercased() else {
+			print("번호가 잘못되었습니다. 다시 선택해주세요.")
+			continue
+		}
+		switch selected {
+			case "1":
+				layout.showCurrentMovie()
+			case "D":
+				layout.showCurrentMovieDetails()
+			case "2":
+				print("영화 예매")
+			case "3":
+				print("구매 확인")
+			case "4":
+				print("푸드 구매")
+			case "0":
+				print(manual.off)
+				break
+			default:
+				continue
+		}
+	}
+}
